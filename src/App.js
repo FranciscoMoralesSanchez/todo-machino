4dc2aaef5b3c63d034478cb8dbc9dc174915c761
morales <franciscomoralessanchez00@gmail.com>
2022-08-28 01:19:18 -0600

    Contando y buscando los todos

--- src/App.js
+++ src/App.js
@@ -7,31 +7,55 @@ import {CreateTodoButton} from './CreateTodoButton';
 // import logo from './logo.svg';
 // import './App.css';
 
-const todos = [
+const defaultTodos = [
   {text:'cortar manzanas', completed: true},
-  {text:'cortar peras', completed: false},
-  {text:'cortar mangos', completed: false},
-  {text:'cortar limones', completed: false}
+  {text:'planchar ropa', completed: false},
+  {text:'ir al gym', completed: false},
+  {text:'hacer la tarea de fisica', completed: false}
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if (!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
+  }
+  
   return (
-  <React.Fragment>
-      <TodoCounter />
-      <TodoSearch/>
+    <React.Fragment>
+      <TodoCounter
+        total={totalTodos}
+        completed={completedTodos}
+      />
+      <TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
 
       <TodoList>
-        {todos.map(todo => (
-          <TodoItem 
-            key={todo.text} 
-            text={todo.text} 
+        {searchedTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
             completed={todo.completed}
           />
         ))}
       </TodoList>
 
-    <CreateTodoButton/>
-  </React.Fragment>
+      <CreateTodoButton />
+    </React.Fragment>
   );
 }
 
